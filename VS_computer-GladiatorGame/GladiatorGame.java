18b19b698a43425731b970025cf0dab0508b4c7d
Aaron Joshua Coronel <150101307+JoshuaDev101@users.noreply.github.com>
2025-10-21 23:49:38 +0800

    Create GladiatorGame.java

---
+++ VS_computer-GladiatorGame/GladiatorGame.java
@@ -0,0 +1,54 @@
+package src;
+import java.util.Scanner;
+public class GladiatorGame {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        
+        while (true){
+            try { 
+                System.out.print("Enter Gladiator 1 Name: ");
+                String name1 = in.nextLine();
+                
+                System.out.print("Enter "  +  name1 + " health points: ");
+                 double health = in.nextDouble();
+       
+                System.out.print("Enter "  +  name1 + " damage points: ");
+                int damage = in.nextInt();                                
+                
+                System.out.print("Enter " + name1  + " defense points: ");
+                int defense = in.nextInt();                
+            
+                System.out.print("Enter " + name1 +  "  initiative:");
+                int initiative = in.nextInt();
+                
+                String enemyName = "Computer";
+                double enemyHealth = 100.0;
+                int randDamage = (int) (Math.random() * (50 - 25 + 1 ) + 25);
+                int randDefense = (int) (Math.random() * (30 - 15 + 1 ) + 15);       
+                int randInit = (int) (Math.random() * (100 - 10 + 1 ) + 10);
+                
+                
+                //creating object
+                Gladiator1 glad1 =
+                    new Gladiator1 (name1, health, damage, defense, initiative);
+          Gladiator1 glad2  =
+                  new Gladiator1(enemyName, enemyHealth, randDamage, randDefense, randInit);
+          
+          
+          System.out.println(
+          Gladiator1.fight(glad1, glad2).getName()
+          + " wins.");
+          
+          break;
+                
+            }catch(Exception e){
+                System.out.println("invalid Input!");
+                in.nextLine();
+            }
+          in.close();
+        }
+          
+    }
+    
+}
