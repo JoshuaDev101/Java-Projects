12e48678bd0ce0d50ca79bfb0fbc8affb40d2e0d
Aaron Joshua Coronel <150101307+JoshuaDev101@users.noreply.github.com>
2025-10-22 17:53:40 +0800

    Add Loop class to print numbers up to 3000

---
+++ Name Loop 3000/Loop.java
@@ -0,0 +1,8 @@
+public class Loop {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 3000; i++){
+            System.out.println(i +" Aaron Joshua Coronel");
+        }
+    }
+}
